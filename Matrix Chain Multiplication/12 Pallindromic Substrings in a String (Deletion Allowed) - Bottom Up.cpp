6145c1c712c8a79a00936a0050540ec90d4abb85
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-01-27 21:57:35 +0530

    Update 12 Pallindromic Substrings in a String (Deletion Allowed) - Bottom Up.cpp

--- Matrix Chain Multiplication/12 Pallindromic Substrings in a String (Deletion Allowed) - Bottom Up.cpp
+++ Matrix Chain Multiplication/12 Pallindromic Substrings in a String (Deletion Allowed) - Bottom Up.cpp
@@ -19,9 +19,6 @@ int main()
                 dp[i][j] = 1;
             }
         }
-        for(int i =  1 ; i < n + 1 ; i++) {
-            dp[i][i] = 1;
-        }
         for(int i =  n - 1 ; i >= 1 ; i--) {
             for(int j = i + 1 ; j < n + 1 ; j++) { 
                     dp[i][j] = ( dp[i + 1][j - 1] == 1 && s[i - 1] == s[j - 1]);
